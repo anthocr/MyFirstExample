45af434370b18379ac936ca789772a66ba46ed27
Anthony C. <antho.cr04@gmail.com>
2026-08-31 11:36:31 -0700

    fixed Nan error

--- main.cpp
+++ main.cpp
@@ -1,25 +1,29 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
-using std::endl;
 using std::cin;
 using std::cout;
+using std::endl;
 
-int main()
-{
-    cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
-    cout << "Hi, please enter two whole numbers: ";
+int main() {
+  cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
+  cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
+  int x, y;
 
-    cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
+  cin >> x >> y;
+
+  cout << "Addition: " << x + y << endl;
+  cout << "Subtraction: " << x - y << endl;
+  cout << "Multiplication: " << x * y << endl;
+  if (y == 0) {
+    cout << "Dividing by zero is not a number." << endl;
+  } else {
     cout << "Division: " << x / y << endl;
     cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+  }
+  cout << "Square Root: " << sqrt(x) << endl;
+  cout << "Square: " << pow(x, y) << endl;
 
-    return 0;
+  return 0;
 }
